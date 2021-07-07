20c416bbc6cf31e76bcd61f2d33e35f9ecd884be
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-07-07 13:27:39 +0200

    codigo comentado

--- apiServer.js
+++ apiServer.js
@@ -21,25 +21,44 @@ const server = app.listen(puerto, () => {
 
 app.use(express.json());
 
+// Adopta un animal segun un json con la siguiente estructura
+/**
+ * {
+ * "dni": dni del usuario
+ * "idAnimal": id del animal a adoptar
+ * }
+ */
 app.put("/adoptaAnimal", async (req, res, next) => {
+  // Recoge el dni y la idAnimal del body
   const { dni, idAnimal } = req.body;
+
+  // Comprobamos si existe el usuario con ese dni y si existe continua
   const usuario = await getUsuarioPorDNI(dni);
 
   if (usuario !== null && typeof usuario.DNI !== "undefined") {
+    // Comprueba si existe un animal sin dueño con la id de animal especificada
     const existeAnimal = await existeAnimalSinDuenyoPorId(idAnimal);
 
+    // Si existeAnimal no es undefined continuara añadiendo el dueño
     if (typeof existeAnimal !== "undefined") {
+      // Añade el dueño
       const resultado = await anyadirDuenyoMascota(usuario.id, idAnimal);
+
+      // Saca el resto de la informacion del animal para ser enviado de una forma bonica
       const infoAnimal = await getAnimalPorId(idAnimal);
+
+      // Si ha podido añadir el dueño al animal envia un mensaje y la informacion del animal adoptado
       if (resultado[0] === 1) {
         res.json({
           mensaje: `Has adoptado a ${infoAnimal.nombre}`,
           Animal: infoAnimal.dataValues,
         });
       } else {
+        // Envia un error general
         next(new Error());
       }
     } else {
+      // Si no existe el animal envia un objeto al manejador de errores con estructura personalizada
       next({
         codigo: 403,
         error: true,
@@ -47,6 +66,7 @@ app.put("/adoptaAnimal", async (req, res, next) => {
       });
     }
   } else {
+    // Si falla el DNI envia un objeto al manejador de errores con estructura personalizada
     next({
       codigo: 403,
       error: true,
@@ -55,10 +75,12 @@ app.put("/adoptaAnimal", async (req, res, next) => {
   }
 });
 
+// Error de ruta no encontrada
 app.use((req, res, next) => {
   res.status(404).send({ error: true, message: "Recurso no encontrado" });
 });
 
+// Manejador de errores
 app.use((err, req, res, next) => {
   const codigo = err.codigo || 500;
   const mensajeError = err.codigo ? err.message : "Ha habido un pete general";
